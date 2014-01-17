37230150e0254d08a94b3c1a8b48ffefe6164a00
Oren Kanner <okanner@gmail.com>
2014-01-17 14:26:30 -0500

    fixed exercise 25, added exercise 28

---
+++ ex28.rb
@@ -0,0 +1,38 @@
+print (true and true), true
+puts
+print (false and true), false
+puts
+print ("test" == "test"), true
+puts
+print (1 == 1 or 2 != 1), true
+puts
+print (true and 1 == 1), true
+puts
+print (false and 0 != 0), false
+puts
+print (true or 1 == 1), true
+puts
+print ("test" == "testing"), false
+puts
+print (1 != 0 and 2 == 1), false
+puts
+print ("test" != "testing"), true
+puts
+print ("test" == 1), false
+puts
+print (not (true and false)), true
+puts
+print (not (1 == 1 and 0 != 1)), false
+puts
+print (not (10 == 1 or 1000 == 1000)), false
+puts
+print (not (1 != 10 or 3 == 4)), false
+puts
+print (not ("testing" == "testing" and "Zed" == "Cool Guy")), true
+puts
+print (1 == 1 and not ("testing" == 1 or 1 == 0)), true
+puts
+print ("chunky" == "bacon" and not (3 == 4 or 3 == 3)), false
+puts
+print (3 == 3 and not("testing" == "testing" or "Ruby" == "fun")), false
+puts
