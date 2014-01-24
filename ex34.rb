8f39a2c893c5d79511f158a622681ce404ac368b
Oren Kanner <okanner@gmail.com>
2014-01-24 13:23:38 -0500

    exercises 34 and 35

---
+++ ex34.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animals[1]} -> python"
+puts "The 1st animal is #{animals.first} -> bear"
+puts "The animal at 3 is #{animals[3]} -> kangaroo"
+puts "The 5th animal is #{animals[4]} -> whale"
+puts "The animal at 2 is #{animals[2]} -> peacock"
+puts "The 6th animal is #{animals[5]} -> platypus"
+puts "The animal at 4 is #{animals[4]} -> whale"
